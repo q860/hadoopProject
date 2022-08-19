b85a0c41e42a19d261ea56f721bb718930424c02
offcn <2696968704@qq.com>
2022-08-19 20:41:00 +0800

    6-commit

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -5,7 +5,7 @@ object Demo {
     def main(args: Array[String]): Unit = {
         //创建程序入口
         val conf: SparkConf = new SparkConf().setAppName("demo").setMaster("local[*]")
-        print("111111111111111111111111")
+        print("1111333333333333")
         val sc = new SparkContext(conf)
         //设置日志级别
         sc.setLogLevel("WARN")
